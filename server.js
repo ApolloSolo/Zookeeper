4c2053bfa6fb810f1c48574082763fd983bfbc8b
ApolloSolo <matt1teixeira@gmail.com>
2022-03-26 14:12:29 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const { animals } = require('./data/animals.json')
 
 function filterByQuery(query, animalsArray) {
